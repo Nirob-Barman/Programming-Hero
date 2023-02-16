b944efb71029710560ee3870d22498c9b3d17d62
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2023-02-16 12:36:26 +0600

    Update expenses.js

--- My Classes/Complete Web Development Course With Jhankar Mahbub/005 Milestone 5: Integrate JavaScript (JavaScript In A Relation)/2023-02-15/005 Module 29.5: Practice task and deploy your site to Surge/Practice/money-master/js/expenses.js
+++ My Classes/Complete Web Development Course With Jhankar Mahbub/005 Milestone 5: Integrate JavaScript (JavaScript In A Relation)/2023-02-15/005 Module 29.5: Practice task and deploy your site to Surge/Practice/money-master/js/expenses.js
@@ -1,18 +1,18 @@
-// document.getElementById('calculate-btn').addEventListener('click', function(){
-//     const incomeAmount = getInputFieldValueById('income');
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const incomeAmount = getInputFieldValueById('income');
 
-//     const foodAmount = getInputFieldValueById('food');
-//     const rentAmount = getInputFieldValueById('rent');
-//     const clothesAmount = getInputFieldValueById('clothes');
+    const foodAmount = getInputFieldValueById('food');
+    const rentAmount = getInputFieldValueById('rent');
+    const clothesAmount = getInputFieldValueById('clothes');
 
-//     const totalExpense = foodAmount + rentAmount + clothesAmount;
+    const totalExpense = foodAmount + rentAmount + clothesAmount;
 
-//     setTextElementValueById('total-expense', totalExpense);
+    setTextElementValueById('total-expense', totalExpense);
 
-//     const currentBalanceAmount = incomeAmount - totalExpense;
+    const currentBalanceAmount = incomeAmount - totalExpense;
 
-//     setTextElementValueById('balance', currentBalanceAmount);
-// })
+    setTextElementValueById('balance', currentBalanceAmount);
+})
 
 document.getElementById('btn-save').addEventListener('click', function(){
     const incomeAmount = getInputFieldValueById('income');
