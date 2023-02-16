53e5e5af8f0d9e2599b5bebfc16b384efb8ca7f7
NiRob BarMan <47166768+rsnirob@users.noreply.github.com>
2023-02-16 12:22:42 +0600

    Add files via upload

---
+++ My Classes/Complete Web Development Course With Jhankar Mahbub/005 Milestone 5: Integrate JavaScript (JavaScript In A Relation)/2023-02-15/005 Module 29.5: Practice task and deploy your site to Surge/Practice/money-master/js/expenses.js
@@ -0,0 +1,25 @@
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const incomeAmount = getInputFieldValueById('income');
+
+    const foodAmount = getInputFieldValueById('food');
+    const rentAmount = getInputFieldValueById('rent');
+    const clothesAmount = getInputFieldValueById('clothes');
+
+    const totalExpense = foodAmount + rentAmount + clothesAmount;
+
+    setTextElementValueById('total-expense', totalExpense);
+
+    const currentBalanceAmount = incomeAmount - totalExpense;
+
+    setTextElementValueById('balance', currentBalanceAmount);
+})
+
+document.getElementById('btn-save').addEventListener('click', function(){
+    const saveAmount = getInputFieldValueById('save');
+    
+    const savingAmount = getTextElementValueById('saving-amount');
+
+    const currentSavingAmount = (temporaryAmount*(savingAmount/100));
+    
+    setTextElementValueById('saving-amount', incomeAmount);
+})
